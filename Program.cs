8710ac52b1d7e3d4155c0dd9b7a85d9dbdfefebf
BIG DON <BIG DON@MSI>
2024-12-26 15:20:48 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Operators_Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;
+            var c = 3;
+
+            Console.WriteLine(a > b); // False
+            Console.WriteLine(a < b); // True
+            Console.WriteLine(a >= b); // False
+            Console.WriteLine(a <= b); // True
+            Console.WriteLine(a == b); // False
+            Console.WriteLine(a != b); // True
+            Console.WriteLine(!(a != b)); // False
+            Console.WriteLine(c > b && c > a); // True
+            Console.WriteLine(c > b && c == a); // False    
+            Console.WriteLine(c > b || c == a); // True
+            Console.WriteLine(!(c > b || c == a)); // False
+
+        }
+    }
+}
